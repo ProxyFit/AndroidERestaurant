24592588ddcc5fd52f96d913b438dd12dbc00a0d
Daniel <daniel.legrand.pro@gmail.com>
2023-03-27 15:27:34 +0200

    27/03 ajout du prix

--- app/src/main/java/fr/isen/legrand/androiderestaurant/CategoryAdapter.kt
+++ app/src/main/java/fr/isen/legrand/androiderestaurant/CategoryAdapter.kt
@@ -1,5 +1,3 @@
-// CategoryAdapter.kt
-
 package fr.isen.legrand.androiderestaurant
 
 import android.content.Context
@@ -11,39 +9,36 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 
-// Définition de la classe "CategoryAdapter" qui étend la classe "RecyclerView.Adapter"
 class CategoryAdapter(private val context: Context, private val menuList: List<Items>) :
     RecyclerView.Adapter<CategoryAdapter.MenuViewHolder>() {
 
-    // Classe interne qui contient les éléments de la vue
     inner class MenuViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val menuTitle: TextView = view.findViewById(R.id.menu_title)
+        val menuPrice: TextView = view.findViewById(R.id.menu_price)
         val menuImage: ImageView = view.findViewById(R.id.menu_image)
     }
 
-    // Cette méthode est appelée lorsque le RecyclerView a besoin d'un nouvel élément de vue
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.item_menu, parent, false)
         return MenuViewHolder(view)
     }
 
-    // Cette méthode est appelée pour remplir les éléments de la vue avec les données du menu
     override fun onBindViewHolder(holder: MenuViewHolder, position: Int) {
-        val menuItem = menuList[position]
-        holder.menuTitle.text = menuItem.nameFr
+        val currentItem = menuList[position]
+        holder.menuTitle.text = currentItem.nameFr
 
-        if (menuItem.images.isNotEmpty()) {
-            Picasso.get()
-                .load(menuItem.images[0])
-                .placeholder(R.drawable.placeholder_image)
-                .error(R.drawable.error_image)
-                .into(holder.menuImage)
+        // Affichage du prix
+        val price = currentItem.prices.firstOrNull()?.price ?: "N/A"
+        holder.menuPrice.text = context.getString(R.string.price_format, price)
+
+        // Chargement et affichage de l'image
+        if (currentItem.images.isNotEmpty()) {
+            Picasso.get().load(currentItem.images[0]).placeholder(R.drawable.placeholder_image).error(R.drawable.error_image).into(holder.menuImage)
         } else {
             holder.menuImage.setImageResource(R.drawable.error_image)
         }
     }
 
-    // Cette méthode retourne le nombre d'éléments de la liste de menus
     override fun getItemCount(): Int {
         return menuList.size
     }
